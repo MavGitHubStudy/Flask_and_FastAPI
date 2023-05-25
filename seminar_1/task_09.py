b95a352b083063b23afe5b7c8800274239b5c669
andrey_milykh <amilykh@mail.ru>
2023-05-25 14:39:31 +0300

    Milykh_Andrey_dz_1

--- seminar_1/task_09.py
+++ seminar_1/task_09.py
@@ -12,6 +12,7 @@
 import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -40,11 +41,18 @@ class Clothes(db.Model):
     isActive = db.Column(db.Boolean, default=True)
     # text = db.Column(db.Text, nullable=False)
 
-    # def __init__(self, title, price, isActive):
-    #     self.title = title
-    #     self.price = price
-    #     self.isActive = isActive
-    #
+    def __repr__(self):
+        return self.title
+
+
+# Обувь
+class Shoes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
     def __repr__(self):
         return self.title
 
@@ -54,6 +62,12 @@ class Clothes(db.Model):
 def index():
     items_clothes = Clothes.query.order_by(Clothes.price).all()
     return render_template('index_shop.html', data=items_clothes)
+
+
+@app.route('/shoes/')
+def shoes():
+    items_shoes = Shoes.query.order_by(Shoes.price).all()
+    return render_template('shoes_shop.html', data=items_shoes)
 
 
 @app.route('/about/')
@@ -77,10 +91,29 @@ def create_clothes():
             db.session.add(item_clothes)
             db.session.commit()
             return redirect('/clothes/')
-        except:
-            return "Получилась ошибка"
+        except IntegrityError:
+            db.session.rollback()
+            return "Получилась ошибка записи в БД"
     else:
         return render_template('create_clothes.html')
+
+
+@app.route('/create_shoes/', methods=['POST', 'GET'])
+def create_shoes():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item_shoes = Shoes(title=title, price=price)
+        try:
+            db.session.add(item_shoes)
+            db.session.commit()
+            return redirect('/shoes/')
+        except IntegrityError:
+            db.session.rollback()
+            return "Получилась ошибка записи в БД"
+    else:
+        return render_template('create_shoes.html')
 
 
 if __name__ == '__main__':
